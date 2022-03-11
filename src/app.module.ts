733049199536eff825c844f1645bdb9af78d295e
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2022-03-11 01:29:39 -0700

    feat: init words, sentences, stories
    The necessary building blocks

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StoriesModule } from './stories/stories.module';
+import { WordsModule } from './words/words.module';
+import { SentencesModule } from './sentences/sentences.module';
 
 @Module({
-  imports: [],
+  imports: [StoriesModule, WordsModule, SentencesModule],
   controllers: [AppController],
   providers: [AppService],
 })
