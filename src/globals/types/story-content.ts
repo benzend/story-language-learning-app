682ea27411e43fe078c3420045bc4cf5a409b1e6
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2022-03-15 01:18:01 -0600

    feat[stories]: story dto created

---
+++ src/globals/types/story-content.ts
@@ -0,0 +1,1 @@
+export type TStoryContent = Array<Array<string>>;
--- src/stories/dto/create-story.dto.ts
+++ src/stories/dto/create-story.dto.ts
@@ -1,1 +1,11 @@
-export class CreateStoryDto {}
+import { TStoryContent } from 'src/globals/types/story-content';
+
+export class CreateStoryDto {
+  id: string;
+  title: string;
+  pageCount: number;
+  content: TStoryContent;
+  dateCreated: Date;
+  dateLastEdited: Date;
+  author: string;
+}
