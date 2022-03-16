6a464f3ed0c271b86eb495b4da2cec0ac3cefee0
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2022-03-16 00:12:35 -0600

    chore: content removed from story dto
    Content will be seperately grabbed per page

--- src/stories/dto/create-story.dto.ts
+++ src/stories/dto/create-story.dto.ts
@@ -1,10 +1,7 @@
-import { TStoryContent } from 'src/globals/types/story-content';
-
 export class CreateStoryDto {
   id: string;
   title: string;
   pageCount: number;
-  content: TStoryContent;
   dateCreated: Date;
   dateLastEdited: Date;
   author: string;
