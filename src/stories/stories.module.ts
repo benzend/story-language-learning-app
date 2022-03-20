b373a329df674282dbbcd2442f0af9cc0da2b602
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2022-03-19 19:57:23 -0600

    chore[stories]: import inmemdbmodule

--- src/stories/stories.module.ts
+++ src/stories/stories.module.ts
@@ -1,7 +1,9 @@
+import { InMemoryDBModule } from '@nestjs-addons/in-memory-db';
 import { Module } from '@nestjs/common';
 import { StoriesController } from './stories.controller';
 
 @Module({
+  imports: [InMemoryDBModule.forRoot({})],
   controllers: [StoriesController],
 })
 export class StoriesModule {}
