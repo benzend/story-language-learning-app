42dc5b74f853ac54401550e9b181d3f16dca87ac
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2022-03-19 23:10:51 -0600

    chore[client]: webpack config added

---
+++ client/webpack/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, '..', 'src', 'index.tsx'),
+  target: 'web',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, '..', 'build'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '..', 'src', 'index.html'),
+    }),
+  ],
+};
